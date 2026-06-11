ea9611953a048478bfce88e9ae937719bcbf39cb
zaladhruvi328-cyber <zaladhruvi328@gmail.com>
2026-06-11 20:36:15 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            while (!Character.isLetterOrDigit(s.charAt(start)) && start < end) {
+                start++;
+            }
+            while (!Character.isLetterOrDigit(s.charAt(end)) && start < end) {
+                end--;
+            }
+            if (start > end || Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
